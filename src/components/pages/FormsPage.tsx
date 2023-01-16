8a529bdb29dd4e2aefee9d74bec6c97a7b09e42c
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2023-01-15 23:50:42 -0300

    Forms pagination

--- src/components/pages/FormsPage.tsx
+++ src/components/pages/FormsPage.tsx
@@ -1,6 +1,8 @@
 import { useApiQuery } from '../../hooks/useApiQuery';
 import {
+  Pagination,
   Paper,
+  Stack,
   Table,
   TableBody,
   TableCell,
@@ -9,6 +11,7 @@ import {
   TableRow,
 } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
+import { ChangeEvent } from 'react';
 interface Form {
   id: number;
   date: string;
@@ -30,40 +33,52 @@ export function FormsPage() {
   const [params, setSearchParams] = useSearchParams();
   const page = params.get('page') ?? '1';
   const skip = page === '1' ? 0 : (Number(page) - 1) * 10;
-
   const { data = { forms: [], totalCount: 0 }, isLoading } = useApiQuery<{
     forms: Form[];
     totalCount: number;
   }>(`questionarios?skip=${skip}`);
 
+  const amountOfPages = Math.ceil(data.totalCount / 10);
+
+  const handlePageChange = (_event: ChangeEvent<unknown>, newPage: number) => {
+    setSearchParams({ page: String(newPage) });
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell sx={{ fontWeight: 'bold' }}>ID</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Data</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Nome</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Descrição</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {isLoading && !data.totalCount ? (
-            <TableMessage isLoading={isLoading} />
-          ) : (
-            data.forms.map((form) => (
-              <TableRow key={form.id}>
-                <TableCell>{form.id}</TableCell>
-                <TableCell>
-                  {new Date(form.date).toLocaleDateString()}
-                </TableCell>
-                <TableCell>{form.name}</TableCell>
-                <TableCell>{form.description}</TableCell>
-              </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Stack justifyContent="center" alignItems="center" spacing={4}>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: 'bold' }}>ID</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }}>Data</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }}>Nome</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }}>Descrição</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {isLoading && data.totalCount === 0 ? (
+              <TableMessage isLoading={isLoading} />
+            ) : (
+              data.forms.map((form) => (
+                <TableRow key={form.id}>
+                  <TableCell>{form.id}</TableCell>
+                  <TableCell>
+                    {new Date(form.date).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>{form.name}</TableCell>
+                  <TableCell>{form.description}</TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Pagination
+        count={amountOfPages}
+        page={parseInt(page)}
+        onChange={handlePageChange}
+      />
+    </Stack>
   );
 }
