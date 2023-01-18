4405271941cbb0570f0a73239e7ca01ded1b6984
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2023-01-18 18:33:38 -0300

    Fix form editing

--- src/components/pages/EditFormPage.tsx
+++ src/components/pages/EditFormPage.tsx
@@ -1,21 +1,22 @@
 import { Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { useUser } from '../../hooks/useUser';
-import { CreatedForm, EditForm } from '../EditForm';
+import { EditForm, GetForm } from '../EditForm';
 import { useApiRequest } from '../../hooks/useApiRequest';
 
 export function EditFormPage() {
   const { id } = useParams();
   const { user } = useUser();
 
-  const { data, isLoading } = useApiRequest<CreatedForm>(`questionario/${id}`, {
+  const { data, isLoading } = useApiRequest<GetForm>(`questionario/${id}`, {
     token: user?.token,
   });
 
   if (isLoading) return <Typography>Carregando...</Typography>;
 
   if (user && data) {
-    return <EditForm user={user} defaultValues={data} />;
+    const { id, ...dataWithoutId } = data;
+    return <EditForm user={user} defaultValues={dataWithoutId} />;
   }
 
   return <Typography>Algo deu errado</Typography>;
